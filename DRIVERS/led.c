6d783fcb0a28fccc2c6b9a4d08f72a3c9692b18d
PriyaSelvakumar123 <pavipriya507@gmail.com>
2026-02-09 11:00:31 +0530

    Update led.c

--- DRIVERS/led.c
+++ DRIVERS/led.c
@@ -1,1 +1,15 @@
+#include"gpio.h"
 
+void led_blink(int pin)
+{
+    GPIO_pin_mode(pin,INPUT);
+
+    while(1)
+    {
+        pin_write(pin,HIGH);
+        __delay_ms(500);
+
+        pin_write(pin,LOW);
+        __delay_ms(500);
+    }
+}
