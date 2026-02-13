c64b16accf61b2ad73bbf7e6c0f84119672fd935
PriyaSelvakumar123 <pavipriya507@gmail.com>
2026-02-13 20:31:50 +0530

    Create key pad.h

---
+++ inc/key pad.h
@@ -0,0 +1,8 @@
+#include"gpio.h"
+/* =========================================================
+   KEYPAD
+   ========================================================= */
+
+void keypad_scan_calculator(port n,port m);  //PORT_NAME (KEYPAD),PORT_NAME(SEVEN SEG)
+
+char keypad_scan_phone(void);                //DEFAULT - PORTB
