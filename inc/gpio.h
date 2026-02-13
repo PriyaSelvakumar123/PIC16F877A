1d8d09e82c977b3dbf1344e425d7ecc056e5d5f8
PriyaSelvakumar123 <pavipriya507@gmail.com>
2026-02-13 20:10:37 +0530

    Update gpio.h

--- inc/gpio.h
+++ inc/gpio.h
@@ -1,1 +1,97 @@
 
+GPIO.H
+
+#ifndef GPIO_H
+#define GPIO_H
+
+#include <xc.h>
+
+/* =========================================================
+   MACROS
+   ========================================================= */
+
+#define  SETIN(n,port)    (port |=  (1<<(n)))
+#define  SETOUT(n,port)   (port &= ~(1<<(n)))
+
+#define  SETHIGH(n,port)  (port |=  (1<<(n)))
+#define  SETLOW(n,port)   (port &= ~(1<<(n)))
+
+/* =========================================================
+   TYPE DEFINITIONS
+   ========================================================= */
+
+typedef enum
+{
+    OUTPUT = 0,
+    INPUT  = 1
+} mode_t;
+
+typedef enum
+{
+    HIGH = 1,
+    LOW  = 0
+} state;
+
+typedef enum
+{
+    PORT_A = 0,
+    PORT_B = 1,
+    PORT_C = 2,
+    PORT_D = 3,
+    PORT_E = 4
+} port;
+
+/* =========================================================
+   REGISTER DEFINITIONS
+   ========================================================= */
+
+/* PORTA */
+#define PORTA (*(volatile unsigned char *)0x05)
+#define TRISA (*(volatile unsigned char *)0x85)
+#define ADCON1(*(volatile unsigned char *)0x9F)
+
+/* PORTB */
+#define PORTB (*(volatile unsigned char *)0x06)
+#define TRISB (*(volatile unsigned char *)0x86)
+
+/* PORTC */
+#define PORTC (*(volatile unsigned char *)0x07)
+#define TRISC (*(volatile unsigned char *)0x87)
+
+/* PORTD */
+#define PORTD (*(volatile unsigned char *)0x08)
+#define TRISD (*(volatile unsigned char *)0x88)
+
+/* PORTE */
+#define PORTE (*(volatile unsigned char *)0x09)
+#define TRISE (*(volatile unsigned char *)0x89)
+
+/*SPECIAL FUNCTIONS*/
+#define STATUS       (*(volatile unsigned char *)0x03)
+#define OPTIONAL_REG (*(volatile unsigned char *)0x81)
+
+
+/* =========================================================
+   GLOBAL VARIABLES (DEFINED IN .c)
+   ========================================================= */
+
+extern volatile unsigned char *port_s[];
+extern volatile unsigned char *tris[];
+
+/* =========================================================
+   GPIO & PORT FUNCTIONS
+   ========================================================= */
+
+void GPIO_pin_mode(int a, mode_t b);     //PIN NO , INPUT OR OUTPUT
+void GPIO_pin_write(int a, state b);     //PIN NO , HIGH OR LOW
+int  GPIO_pin_read(int a);               //PIN NO
+
+/* PORT MUST BE PORT_A/PORT_B/PORT_C/PORT_D/PORT_E */
+
+
+void port_mode(port n, mode_t m);        //PORT_NAME, INPUT OR OUTPUT
+void port_write(port n, state m);        //PORT_NAME, HIGH OR LOW
+int  port_read(port n);                  //RA4 CAN NOT BE INPUT AND OUTPUT //
+
+
+
