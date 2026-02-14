44eb3366876b8df2ed35d0804daaed0398d4d3e0
PriyaSelvakumar123 <pavipriya507@gmail.com>
2026-02-14 17:28:32 +0530

    Update gpio.c

--- src/gpio.c
+++ src/gpio.c
@@ -1,170 +1,230 @@
+#include "gpio.h"
+#include <stdint.h>
+
 /* =========================================================
-   GPIO & PIN FUNCTIONS
+   GLOBAL ARRAY DEFINITIONS
    ========================================================= */
 
-void GPIO_pin_mode(int a,mode_t b)
+volatile uint8_t *port_registers[] =
 {
-    if(a>=33&&a<=40)                    //PORTB PINS
-    {
-        if(b==OUTPUT)
-            SETOUT(a-33,TRISB);
-        else
-            SETIN(a-33,TRISB);
-    }
-    else if(a>=2&&a<=7)                 //PORTA PINS
-    {
-        ADCON1=0x06;
-        if(b==OUTPUT)
-            SETOUT(a-2,TRISA);
-        else
-            SETIN(a-2,TRISA);
-    }
-    else if((a>14&&a<19)||(a>22&&a<27)) //PORTC PINS
-    {
-        if(a<19)a=a-15;
-        else a=a-19;
+    &PORTA,
+    &PORTB,
+    &PORTC,
+    &PORTD,
+    &PORTE
+};
 
-        if(b==OUTPUT)
-            SETOUT(a,TRISC);
-        else
-            SETIN(a,TRISC);
-    }
-    else if((a>18&&a<23)||(a>26&&a<31))  //PORTD PINS
-    {
-        if(a<23)a=a-19;
-        else a=a-23;
+volatile uint8_t *tris_registers[] =
+{
+    &TRISA,
+    &TRISB,
+    &TRISC,
+    &TRISD,
+    &TRISE
+};
 
-        if(b==OUTPUT)
-            SETOUT(a,TRISD);
-        else
-            SETIN(a,TRISD);
-    }
-    else if(a>7&&a<11)                 //PORTE PINS
+/* =========================================================
+   GPIO PIN MODE FUNCTION
+   ========================================================= */
+
+void GPIO_pin_mode(uint8_t pin_number, mode_t pin_mode)       
+{
+   
+    if(pin_number >= 2 && pin_number <= 7)                    // PORTA
     {
-        ADCON1=0x06;
-        if(b==OUTPUT)
-            SETOUT(a-8,TRISE);
+        ADCON1 = 0x06;                                        // ADCON1 USED FOR PORTE AS DIGITAL INPUT OR OUTPUT
+
+
+        if(pin_mode==OUTPUT)
+            SETOUT(pin_number - 2,TRISA);
         else
-            SETIN(a-8,TRISE);
+            SETIN(pin_number - 2,TRISA);
+    }
+    else if(pin_number >= 33 && pin_number <= 40)            // PORTB               
+    {
+        if(pin_mode==OUTPUT)
+            SETOUT(pin_number - 33,TRISB);
+        else
+            SETIN(pin_number - 33,TRISB);
+    }
+
+    else if((pin_number > 14 && pin_number < 19) ||         // PORTC
+            (pin_number > 22 && pin_number < 27))
+    {
+        uint8_t bit;
+       
+        if(pin_number < 19)
+            bit = pin_number - 15;
+        else
+            bit = pin_number - 19;
+
+        if(pin_mode == OUTPUT)
+            SETOUT(bit,TRISC);
+        else
+            SETIN(bit,TRISC);
+    }
+
+    else if((pin_number > 18 && pin_number < 23) ||       // PORTD
+            (pin_number > 26 && pin_number < 31))
+    {
+        uint8_t bit;
+
+        if(pin_number < 23)
+            bit = pin_number - 19;
+        else
+            bit = pin_number - 23;
+
+        if(pin_mode == OUTPUT)
+            SETOUT(bit,TRISD);
+        else
+            SETIN(bit,TRISD);
+    }
+
+    else if(pin_number > 7 && pin_number < 11)         // PORTE
+    {
+        ADCON1 = 0x06;                                 // ADCON1 USED FOR PORTE AS DIGITAL INPUT OR OUTPUT
+
+        if(pin_mode == OUTPUT)
+            SETOUT(pin_number - 8, TRISE);
+        else
+            SETIN(pin_number - 8, TRISE);
     }
 }
 
-void GPIO_pin_write(int a,state b)
+/* =========================================================
+   GPIO PIN WRITE FUNCTION
+   ========================================================= */
+
+void GPIO_pin_write(uint8_t pin_number, state pin_state)
 {
-    if(a>=33&&a<=40)
+    if(pin_number >= 33 && pin_number <= 40)
     {
-        if(b==LOW)
-            SETLOW(a-33,PORTB);
+        if(pin_state == LOW)
+            SETLOW(pin_number - 33, PORTB);
         else
-            SETHIGH(a-33,PORTB);
+            SETHIGH(pin_number - 33, PORTB);
     }
-    else if(a>=2&&a<=7)
-    {
-        if(b==LOW)
-            SETLOW(a-2,PORTA);
-        else
-            SETHIGH(a-2,PORTA);
-    }
-    else if((a>14&&a<19)||(a>22&&a<27))
-    {
-        if(a<19)a=a-15;
-        else a=a-19;
 
-        if(b==LOW)
-            SETLOW(a,PORTC);
-        else
-            SETHIGH(a,PORTC);
-    }
-    else if((a>18&&a<23)||(a>26&&a<31))
+    else if(pin_number >= 2 && pin_number <= 7)
     {
-        if(a<23)a=a-19;
-        else a=a-23;
+        if(pin_state == LOW)
+            SETLOW(pin_number - 2, PORTA);
+        else
+            SETHIGH(pin_number - 2, PORTA);
+    }
 
-        if(b==LOW)
-            SETLOW(a,PORTD);
-        else
-            SETHIGH(a,PORTD);
-    }
-    else if(a>7&&a<11)
+    else if((pin_number > 14 && pin_number < 19) ||
+            (pin_number > 22 && pin_number < 27))
     {
-        if(b==LOW)
-            SETLOW(a-8,PORTE);
+        uint8_t bit;
+
+        if(pin_number < 19)
+            bit = pin_number - 15;
         else
-            SETHIGH(a-8,PORTE);
+            bit = pin_number - 19;
+
+        if(pin_state == LOW)
+            SETLOW(bit, PORTC);
+        else
+            SETHIGH(bit, PORTC);
+    }
+
+    else if((pin_number > 18 && pin_number < 23) ||
+            (pin_number > 26 && pin_number < 31))
+    {
+        uint8_t bit;
+
+        if(pin_number < 23)
+            bit = pin_number - 19;
+        else
+            bit = pin_number - 23;
+
+        if(pin_state == LOW)
+            SETLOW(bit, PORTD);
+        else
+            SETHIGH(bit, PORTD);
+    }
+
+    else if(pin_number > 7 && pin_number < 11)
+    {
+        if(pin_state == LOW)
+            SETLOW(pin_number - 8, PORTE);
+        else
+            SETHIGH(pin_number - 8, PORTE);
     }
 }
 
-int pin_read(int a)
-{
-    if(a >=33 && a <= 40)
-    {
-        return(PORTB &(1 << (a - 33)))? 1 : 0;
-    }
-    else if(a>=2 && a<=7)
-    {
-        return(PORTA & (1<<(a - 2)))?1 : 0;
-    }
-    else if(((a>14) && (a<19)) || ((a>22) && (a<27)))
-    {
-        if(a<19)
-            a = a - 15;
-        else
-            a = a - 19;
+/* =========================================================
+   GPIO PIN READ FUNCTION
+   ========================================================= */
 
-        return (PORTC & (1 << a)) ? 1 : 0;
-    }
-    else if(((a>18) && (a<23)) || ((a>26) && (a<31)))
+uint8_t GPIO_pin_read(uint8_t pin_number)
+{
+    if(pin_number >= 33 && pin_number <= 40)
+        return (PORTB & (1 << (pin_number - 33))) ? 1 : 0;
+
+    else if(pin_number >= 2 && pin_number <= 7)
+        return (PORTA & (1 << (pin_number - 2))) ? 1 : 0;
+
+    else if((pin_number > 14 && pin_number < 19) ||
+            (pin_number > 22 && pin_number < 27))
     {
-        if(a < 23)
-            a = a - 19;
+        uint8_t bit;
+
+        if(pin_number < 19)
+            bit = pin_number - 15;
         else
-            a = a - 23;
-        return (PORTD & (1 << a)) ? 1 : 0;
+            bit = pin_number - 19;
+
+        return (PORTC & (1 << bit)) ? 1 : 0;
     }
-    else if((a>7) && (a<11))
+
+    else if((pin_number > 18 && pin_number < 23) ||
+            (pin_number > 26 && pin_number < 31))
     {
-        return (PORTE & (1 << (a - 8))) ? 1 : 0;
+        uint8_t bit;
+
+        if(pin_number < 23)
+            bit = pin_number - 19;
+        else
+            bit = pin_number - 23;
+
+        return (PORTD & (1 << bit)) ? 1 : 0;
     }
+
+    else if(pin_number > 7 && pin_number < 11)
+        return (PORTE & (1 << (pin_number - 8))) ? 1 : 0;
+
     return 0;
 }
 
-
 /* =========================================================
-   GPIO & PORT FUNCTIONS
+   PORT FUNCTIONS
    ========================================================= */
 
-void port_mode(port n,mode_t m)
+void port_mode(port port_name, mode_t port_mode)
 {
-    volatile unsigned char *t1;
-    t1 = tris[n];
-    if(m== INPUT)
-    {
-        *t1=0xFF;
-    }
-    if(m == OUTPUT)
-    {
-        *t1=0x00;
-    }
+    volatile uint8_t *tris_register = tris_registers[port_name];
+
+    if(port_mode == INPUT)
+        *tris_register = 0xFF;
+    else
+        *tris_register = 0x00;
 }
 
-void port_write(port n,state m)
+void port_write(port port_name, state port_state)
 {
-    volatile unsigned char *p1;
-    p1 = port_s[n];
-    if(m== HIGH)
-    {
-        *p1=0xFF;
-    }
-    if(m == LOW)
-    {
-        *p1=0x00;
-    }
+    volatile uint8_t *port_register = port_registers[port_name];
+
+    if(port_state == HIGH)
+        *port_register = 0xFF;
+    else
+        *port_register = 0x00;
 }
 
-int port_read(port n)
+uint8_t port_read(port port_name)
 {
-    if(n & 0xFF)
-        return 1;
-    else if(n & 0x00 )
-        return 0;
+    volatile uint8_t *port_register = port_registers[port_name];
+
+    return *port_register;
 }
