e1a553a645d5616b9b44c972d073819cd4188dec
PriyaSelvakumar123 <pavipriya507@gmail.com>
2026-02-15 10:02:20 +0530

    Update seven segment.h

--- inc/seven segment.h
+++ inc/seven segment.h
@@ -1,12 +1,7 @@
-#include"gpio.h"
+#include "gpio.h"
 
-/* =========================================================
-   SEVEN SEGMENT
-   ========================================================= */
+void seven_segment_ANODE(uint_8 a, port n);
 
-void seven_segment_ANODE(unsigned char a, port n);      // HEX - 0 - F , PORT_NAME (SEVEN SEG)
+void seven_segment_CATHODE(uint_8 a, port n);
 
-void seven_segment_CATHODE(unsigned char a, port n);
-
-void four_digit_segment(unsigned int num, port seg, port mux);   // DEC 0 - 9 ,PORT_NAME (SEVEN SEG) ,PORT_NAME (SELECT LINE - 4)
-
+void four_digit_segment(uint_16 num, port seg, port mux);
