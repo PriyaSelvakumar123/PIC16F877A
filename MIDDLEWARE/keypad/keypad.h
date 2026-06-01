59c973cb25c172335adfcbb092a208bf2c891b47
PriyaSelvakumar123 <pavipriya507@gmail.com>
2026-06-01 14:30:29 +0530

    Update keypad.h

--- MIDDLEWARE/keypad/keypad.h
+++ MIDDLEWARE/keypad/keypad.h
@@ -1,1 +1,9 @@
+#include"gpio.h"
+/* =========================================================
+   KEYPAD
+   ========================================================= */
+
+void keypad_scan_calculator(port keypad,port sevenseg);  //PORT_NAME (KEYPAD),PORT_NAME(SEVEN SEG)
+
+uint_8 keypad_scan_phone(void);                //DEFAULT - PORTB
 
