8708012d0cfbc2dad7bd4b4a112780b063472703
PriyaSelvakumar123 <pavipriya507@gmail.com>
2026-06-01 13:58:38 +0530

    Delete inc directory

--- inc/key pad.h
+++
@@ -1,8 +0,0 @@
-#include"gpio.h"
-/* =========================================================
-   KEYPAD
-   ========================================================= */
-
-void keypad_scan_calculator(port keypad,port sevenseg);  //PORT_NAME (KEYPAD),PORT_NAME(SEVEN SEG)
-
-uint_8 keypad_scan_phone(void);                //DEFAULT - PORTB
--- inc/seven segment.h
+++
@@ -1,7 +0,0 @@
-#include "gpio.h"
-
-void seven_segment_ANODE(uint_8 a, port n);
-
-void seven_segment_CATHODE(uint_8 a, port n);
-
-void four_digit_segment(uint_16 num, port seg, port mux);
