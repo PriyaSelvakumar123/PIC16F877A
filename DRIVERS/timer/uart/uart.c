4c6408e076aa08100c8dc20e8340a1c5dd5fcf72
PriyaSelvakumar123 <pavipriya507@gmail.com>
2026-06-04 16:54:40 +0530

    Create uart.c

---
+++ DRIVERS/timer/uart/uart.c
@@ -0,0 +1,35 @@
+#include "uart..h"
+
+void UART_Init(uint32_t baudrate)
+{
+    SPBRG = (_XTAL_FREQ / (16UL * baudrate)) - 1;
+
+    TXSTAbits.SYNC = 0;    //Asynchronous
+    TXSTAbits.BRGH = 1;    //HIGH SPEED
+
+    RCSTAbits.SPEN = 1;    //serial port RC6 AND RC7 
+
+    TXSTAbits.TXEN = 1;    //TRANSMITER ENABLE
+    RCSTAbits.CREN = 1;    //RECIVER ENABLE
+}
+
+void UART_WriteChar(char data)
+{
+    while(!PIR1bits.TXIF);   // TRANSMITER ACKNOWLEDGEMENT
+    TXREG = data;
+}
+
+char UART_Read(void)
+{
+    while(!PIR1bits.RCIF);   // RECIVER ACKNOWLEDGEMENT
+    return RCREG;
+}
+
+void UART_WriteString(const char *str)
+{
+    while(*str)
+    {
+        UART_WriteChar(*str);
+        str++;
+    }
+}
