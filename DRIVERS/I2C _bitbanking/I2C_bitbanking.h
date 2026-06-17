a101a51a7a6f3d2e10b023d834e4d8fbcfd5f432
PriyaSelvakumar123 <pavipriya507@gmail.com>
2026-06-17 20:56:16 +0530

    Update I2C_bitbanking.h

--- DRIVERS/I2C _bitbanking/I2C_bitbanking.h
+++ DRIVERS/I2C _bitbanking/I2C_bitbanking.h
@@ -1,1 +1,26 @@
+#ifndef I2C_H
+#define I2C_H
 
+#include <stdint.h>
+#include "gpio.h"
+#include "timer.h"
+
+#define _XTAL_FREQ 20000000UL
+
+#define SCL_PIN 0   // RB0
+#define SDA_PIN 1   // RB1
+
+void I2C_init(void);
+void I2C_start(void);                   
+void I2C_stop(void);
+
+void scl_high(void);
+void scl_low(void);
+void sda_high(void);
+void sda_low(void);
+
+unsigned char I2C_write_byte(unsigned char data);
+void I2C_writechar(unsigned char address, unsigned char data);
+void I2C_writestring(unsigned char address, char *data);
+
+#endif
